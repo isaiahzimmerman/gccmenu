1202994cab52a3086a007d3b1be5ef3769f77b19
zimmycrafter <zimmycrafterplaysminecraftpepc@gmail.com>
2023-10-03 12:40:55 -0400

    add romans 8

---
+++ main.js
@@ -0,0 +1,37 @@
+document.title = "GCC Menu 9/17/23"
+
+
+/*[section
+     [meal]: breakfast, lunch dinner
+     [choices]]
+*/
+//Week of
+
+weeklyMenu = [
+    {
+        loc: "Main Station",
+        meals: ["lunch", "dinner"],
+        choices: [
+            ["Ancho Grilled Chicken Brown Rice Bowl"],
+            ["Pacific Poke Bowl"],
+            ["Vegetable Biriyani", "Moroccan Braised Beef Tips"],
+            ["Harissa Chicken Rice Bowl"],
+            ["Butternut Squash Chickpea Curry Basmati Rice"],
+            false,
+            false
+        ]
+    },
+    {
+        loc: "Inspired Eats",
+        meals: ["lunch"],
+        choices: [
+            ["Mushroom White Bean Meatballs - Allergen", "Spaghetti - Allergen Friendly", "Marinara Sauce", "Sauteed Spinach"],
+            ["Honey Mustard Baked Chicken", "Lentils Diavolo", "Carrots"],
+            ["Mexican Braised Beef", "Mexican Chopped Salad",  "Potato Salad w Smoky Citrus", "Vinaigrette"],
+            ["Herbed Chicken Stew & Dumplings", "Sauteed Peppers and Peas"],
+            ["Chicken & Butternut Squash Curry", "Indian Pumpkin Butter Chickpeas - Allergen Friendly", "Basmati Rice Pilaf"],
+            false,
+            false
+        ]
+    }
+]
